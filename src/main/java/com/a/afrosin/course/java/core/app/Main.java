1c4cf606a20e6f13f1e05b80f4547541ec6d7819
Alexey <ayuafrosin@gmail.com>
2020-09-14 19:45:05 +0400

    Задания к уроку 8. Добавлено: - рисование "нолика" для хода соперника; - вывод сообщения о победе; - отрисовка главного окна по середине экрана; - рисование победной линии.

--- src/main/java/com/a/afrosin/course/java/core/app/Main.java
+++ src/main/java/com/a/afrosin/course/java/core/app/Main.java
@@ -2,5 +2,7 @@ package com.a.afrosin.course.java.core.app;
 
 public class Main {
     public static void main(String[] args) {
+        GameWindow gameWindow = new GameWindow();
+//        gameWindow.
     }
 }
